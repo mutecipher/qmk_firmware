3ecb98d9820e97f073b9cc7d00ad93b179c74100
Cory Hutchison <cory_hutchison@icloud.com>
2021-10-11 17:04:42 -0600

    [keymap] Add `mutecipher` keymap
    Includes keymappings for:
    * crkbd, and
    * lily58
    
    Signed-off-by: Cory Hutchison <cory_hutchison@icloud.com>

---
+++ users/mutecipher/mutecipher.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define VOLDOWN KC__VOLDOWN
+#define VOLUP KC__VOLUP
+#define MUTE KC__MUTE
+#define BRDOWN KC_BRIGHTNESS_DOWN
+#define BRUP KC_BRIGHTNESS_UP
+
+enum layer_number {
+    _QWERTY = 0,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
+
